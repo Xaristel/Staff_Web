0a3224aebbac011e09de917c4575be44935cd131
Xaristel <20muxa00@bk.ru>
2022-10-11 15:14:49 +0300

    Add project files.

---
+++ Staff_Web/Data/DBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Staff_Web.Models;
+
+namespace Staff_Web.Data
+{
+    public class DBContext : DbContext
+    {
+        public DBContext(DbContextOptions<DBContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employee { get; set; } = default!;
+    }
+}
---
+++ Staff_Web/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Staff_Web.Models
+{
+    [Table("Employees")]
+    [Index("FIO", IsUnique = true)]
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FIO { get; set; } = "";
+        public string Position { get; set; } = "";
+    }
+}
